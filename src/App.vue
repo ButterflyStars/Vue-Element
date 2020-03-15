28fb7d9d39c3a1e2f49169da8dacf8720fa196f2
ButterflyStars <1498575492@qq.com>
2020-03-16 04:38:32 +0800

    进行了登录页面的美化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-
     <router-view />
   </div>
 </template>
@@ -19,6 +17,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*调整整个页面视图位置*/
+  margin-top: -8px;
+  margin-left: -2px;
 }
 </style>
